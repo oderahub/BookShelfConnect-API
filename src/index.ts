5bc149f027a228c8fc01104b8d4b269cae18fe6e
oderahub <okere_chidera@yahoo.com>
2025-02-08 10:56:50 +0100

    chore: setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  })
+)
+
+app.disable('x-powered-by')
 
 app.get('/', (req, res) => {
   res.json({ message: 'QuikDb capstone project' })
