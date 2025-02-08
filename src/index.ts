8b48792fa7a2f6ced2caae2b12a70e35f1c82aa9
oderahub <okere_chidera@yahoo.com>
2025-02-08 10:41:08 +0100

    chore: setup

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
-const express = require('express')
+import express from 'express'
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
-
-
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.json({ message: 'QuikDb capstone project' })
+  res.json({ message: 'QuikDb capstone project' })
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT}`)
 })
